fbe533712b4dfeea321f735e885ecbd1e63e07a5
aikikode <aikikode@gmail.com>
2013-12-06 19:00:40 +0400

    Add 1.2 chapter

---
+++ 1.11.scm
@@ -0,0 +1,26 @@
+#!/usr/bin/env guile
+!#
+; -*-scheme-*-
+
+; A function f is defined by the rule that f(n) = n if n<3 and f(n) = f(n - 1)
+; + 2f(n - 2) + 3f(n - 3) if n> 3. Write a procedure that computes f by means
+; of a recursive process. Write a procedure that computes f by means of an
+; iterative process.
+
+(display "Features:\nImplementation of the function: f(n) = n if n<3 and f(n) = f(n - 1) + 2f(n - 2) + 3f(n - 3) otherwise\n(f-rec n) - recursive function\n(f-iter n) - iterative same function\n\n")
+
+(define (f-rec n)
+ (if (< n 3)
+  n
+  (+ (f-rec (- n 1)) (* 2 (f-rec (- n 2))) (* 3 (f-rec (- n 3))))))
+
+(define (f-iter n)
+
+  (define (cc a b c count)
+    (if (< count 3)
+      c
+      (cc b c (+ c (* 2 b) (* 3 a)) (- count 1))))
+
+  (if (< n 3)
+    n
+    (cc 0 1 2 n)))
---
+++ 1.2.2.scm
@@ -0,0 +1,24 @@
+#!/usr/bin/env guile
+!#
+; -*-scheme-*-
+
+(display "Features:\n(count-change amount) - count the number of ways to change this amount of money\n\n")
+
+(define (count-change amount)
+  (cc amount 5))
+
+(define (cc amount kinds-of-coins)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (= kinds-of-coins 0)) 0)
+        (else (+ (cc amount
+                     (- kinds-of-coins 1))
+                 (cc (- amount
+                        (first-denomination kinds-of-coins))
+                     kinds-of-coins)))))
+
+(define (first-denomination kinds-of-coins)
+  (cond ((= kinds-of-coins 1) 1)
+        ((= kinds-of-coins 2) 5)
+        ((= kinds-of-coins 3) 10)
+        ((= kinds-of-coins 4) 25)
+        ((= kinds-of-coins 5) 50)))
